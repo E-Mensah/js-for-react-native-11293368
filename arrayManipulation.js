07762034aec4f293bee35647f41cbb2af291fafc
Mensah <mensaherskine@gmail.com>
2024-05-19 22:08:56 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,4 +5,24 @@ function processArray(arrayElement) {
     );
   }
 
+//Task 2
+
+function formatArrayStrings (stringArray, numbersArray){
+    let processedArray = processArray(numbersArray);
+    if (stringArray.length !== processedArray.length){
+        return "Arrays are not of the same length !!";
+    }
+
+    let formattedStrings = stringArray.map((string, index) => {
+        if (processedArray[index] % 2 === 0) {
+            return string.toUpperCase(); 
+        } else {
+            return string.toLowerCase(); 
+        }
+    });
+
+    return formattedStrings;
+}
+
+export const formatArrayStrings = formatArrayStrings;
 export const processArray = processArray;
